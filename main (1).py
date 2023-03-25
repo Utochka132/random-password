89c2a2ba2b077ce43697927a7c85bbfe5b1a6b46
Utochka132 <127588363+Utochka132@users.noreply.github.com>
2023-03-25 12:08:22 +0400

    Update main (1).py

--- main (1).py
+++ main (1).py
@@ -1,9 +1,23 @@
-import random
-element = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-password = int(input("какая длинна пароля?"))
+import discord
+from discord.ext import commands
+from bot_logic import gen_pass
 
-password_1 = ""
-for i in range (password):
-    password_1+=random.choice(element)
-print(password_1)
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Привет! Я бот {bot.user}!')
+
+@bot.command()
+async def pasw(ctx):
+    await ctx.send(gen_pass(10))
+
+bot.run("Токен")
 
