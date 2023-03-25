d7707bc28f66372505178391a291f5d3df09acb5
Utochka132 <127588363+Utochka132@users.noreply.github.com>
2023-03-25 12:15:07 +0400

    Update main (1).py

--- main (1).py
+++ main (1).py
@@ -18,6 +18,22 @@ async def hello(ctx):
 @bot.command()
 async def pasw(ctx):
     await ctx.send(gen_pass(10))
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Привет! Я бот {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
 
 bot.run("Токен")
 
